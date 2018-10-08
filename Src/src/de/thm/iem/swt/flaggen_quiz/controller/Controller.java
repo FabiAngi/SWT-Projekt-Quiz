bbd2029ae98e8b4571b98252074dc660e92ce307
nnnik <nnnik@web.de>
2018-10-08 11:14:46 +0200

    Revert "Elaboration"

--- Src/src/de/thm/iem/swt/flaggen_quiz/controller/Controller.java
+++
@@ -1,9 +0,0 @@
-package de.thm.iem.swt.flaggen_quiz.controller;
-
-import de.thm.iem.swt.flaggen_quiz.view.View;
-
-public interface Controller {
-	
-	public void attatchToView(View view);
-	
-}
--- Src/src/de/thm/iem/swt/flaggen_quiz/view/View.java
+++
@@ -1,9 +0,0 @@
-package de.thm.iem.swt.flaggen_quiz.view;
-
-import java.util.Map;
-import javafx.scene.Node;
-
-public interface View {
-	public Node getContent();
-	public Map<String,Node> getControls();
-}
